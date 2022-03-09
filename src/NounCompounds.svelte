e2799911128b926da70671e26c0d6b98a5416846
Steve Hanlon <rshanlon@gmail.com>
2022-03-08 16:54:22 -0800

    Added HTML and text content to NounCompounds component

--- src/NounCompounds.svelte
+++ src/NounCompounds.svelte
@@ -1,31 +1,35 @@
 <script></script>
 
-<!-- Section 7 -->
+<!-- Section 7   <span class="italic-w-ipg"></span> -->
 <section id="section-7" class="section-divider-ipg">
   <h3 class="heading-2-ipg">Section Seven</h3>
   <h3 class="heading-3-ipg">Compounds of nouns (<span class="italic-w-ipg">samāsa</span>)</h3>
   <p class="paragraph-1-ipg">
-    <span class="italic-w-ipg"></span>
+    When nouns join together with two or more words they are called nominal compounds or <span class="italic-w-ipg">samāsa</span>. The first member of a compound can be another noun, an adjective, an adverb, a pronoun, a verbal form, or a numeral. In general the last member of the compound gets inflected according to its declension while the other members keep their stem form. When joined the usual <span class="italic-w-ipg">sandhi</span>-formations of lengthening, shortening, elision etc. may need to get applied.    
   </p>
 
   <p class="paragraph-1-ipg">
-
+    Nominal compounds take their name according to the procedure or the form they take and their function when combined. Thus two or more members instead of being connected with the particle ca getting joined together and function as copulative compound are called a) <span class="italic-w-ipg">dvanda samāsa</span>. A combination where one member, usually the second modifies the first and functions as descriptive compound is called b) <span class="italic-w-ipg">kammadhāraya samāsa</span>. In cases where the first member depends on the second (dependent compounds) it is named c) <span class="italic-w-ipg">tappurisa samāsa</span>. In cases where nominal compounds convey an adjective sense its term is d) <span class="italic-w-ipg">bahubbīhi samāsa</span>. Compounds that function as adverbial compounds or indeclinables, generally constructed from prefix + noun or indeclinable + noun are called e) <span class="italic-w-ipg">avyayībhāva samāsa</span>. Here the first member predominates the second. One more group denoting a period of time with the first member being a numeral is called f) <a href="#section7-note-41" class="notes-ipg" id="section7-note-41-rtn"><span class="italic-w-ipg">digu samāsa</span></a><sup class="sup-spacing-ipg">41</sup>. 
   </p>
 
   <p class="paragraph-1-ipg">
+    Examples:<br>
+    a)	<span class="italic-w-ipg bold-w-ipg">dvanda samāsa</span>: <span class="italic-w-ipg">udayabbayaṃ; nāmarūpaṃ; ahivicchikā, punappunaṃ, hattapādā; samaṇabrāhmaṇā</span><br>
 
+    b)	<span class="italic-w-ipg bold-w-ipg">kammadhāraya samāsa</span>: <span class="italic-w-ipg">tejodhatu; cakkaratanaṃ; pañcakkhaṅḍa; viriyindriyaṃ; dīgharattaṃ; pubbajāti</span><br>
+    
+    c)	<span class="italic-w-ipg bold-w-ipg">tappurisa samāsa</span>: <span class="italic-w-ipg">lokavidū; sīlasampanno; bhikkhusaṅgho; dukkhasamudayo; kālaṃkato; sabbaññu</span><br>
+    
+    d)	<span class="italic-w-ipg bold-w-ipg">bahubbīhi samāsa</span>: <span class="italic-w-ipg">rukkho ucchinnamūlo; rukkhamūliko; micchādiṭṭhiko; pāpakammino; manopubbaṅgamā dhammā manoseṭṭhā</span><br>
+    
+    e)	 <span class="italic-w-ipg bold-w-ipg">avyayībhāva samāsa</span>: <span class="italic-w-ipg">ajjhattaṃ; anulomaṃ; paṭilomaṃ; yathābhūtaṃ; yāvajīvaṃ; paccattaṃ</span><br>
+    
+    f)	<span class="italic-w-ipg bold-w-ipg">digu samāsa</span>: <span class="italic-w-ipg">sattāhaṃ; saḷāyatanaṃ; catuddisaṃ; sattavassāni; tīhaṃ; tīcīvaraṃ</span> <br>
   </p>
 
-  <p class="paragraph-1-ipg">
-
-  </p>
-
-  <p class="paragraph-1-ipg">
-
-  </p>
   <!-- footnotes section 7 -->
   <ul class="footnotes-container-ipg">
-    <li id="section7-note-41" class="footnotes-ipg"><sup>41</sup>  <a class="backarrow-ipg" href="#section7-note-41-rtn">↩</a></li>
+    <li id="section7-note-41" class="footnotes-ipg"><sup>41</sup>Detailed explanation of the groups of samāsa and classifications see Perinola; Pali Grammar, Chapter IX.   <a class="backarrow-ipg" href="#section7-note-41-rtn">↩</a></li>
   </ul>
 </section>
 
