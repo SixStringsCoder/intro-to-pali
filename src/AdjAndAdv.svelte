deb559dbb3a040070434d10ca8b3f608b5ad6a88
Steve Hanlon <rshanlon@gmail.com>
2022-03-16 21:55:19 -0700

    Finished content for AdjAndAdv component

--- src/AdjAndAdv.svelte
+++ src/AdjAndAdv.svelte
@@ -75,6 +75,74 @@
   </div>
   <!-- /table -->
 
+  <p class="paragraph-1-ipg">
+    In addition to the endings: <span class="italic-w-ipg">-a; -i; -u; -in; -ant; -mant; -vant</span>;
+  </p>
+  <ul class="para-list-ipg"> 
+    <li>the following suffixes are of common occurrence: -<span class="italic-w-ipg">ka</span> (<span class="italic-w-ipg">garuka</span> – heavy); -<span class="italic-w-ipg">ika</span> (<span class="italic-w-ipg">kāyika</span> – physical); -<span class="italic-w-ipg">aka</span> (<span class="italic-w-ipg">jiṇṇaka</span> – old – derived from p.p. <span class="italic-w-ipg">jiṇṇa</span>);</li>
+    <li>Possession express the suffixes: -<span class="italic-w-ipg">vā</span> (<span class="italic-w-ipg">guṇavā</span> – virtuous, with qualities) -<span class="italic-w-ipg">vī</span> (<span class="italic-w-ipg">medhāvī</span> – wise, with wisdom).</li> 
+  </ul>
+
+  <p class="paragraph-1-ipg">
+    Adjectives denote comparisons by adding -tara for the comparative and (in rare cases only) -<span class="italic-w-ipg">tama</span> for the superlative:
+  </p>
+  <ul class="para-list-ipg"> 
+    <li>The comparative suffixes -<span class="italic-w-ipg">tara</span>: <span class="italic-w-ipg">piyatara</span> – more dear; <span class="italic-w-ipg">dūratara</span> – farther; <span class="italic-w-ipg">pāpatara</span> – more evil.</li>
+    <li>The superlative uses the suffixes: -<span class="italic-w-ipg">tama</span>: <span class="italic-w-ipg">uttama</span> – the highest; <span class="italic-w-ipg">piyatama</span> – dearest; <span class="italic-w-ipg">dūratama</span> – farthest.</li> 
+  </ul>
+  
+  <p class="paragraph-1-ipg">
+    Alternatively the following suffix:  
+  </p>
+  <ul class="para-list-ipg"> 
+    <li>-<span class="italic-w-ipg">iya</span> is used for the comparative: <span class="italic-w-ipg">pāpiya</span> – more evil; <span class="italic-w-ipg">khippiya</span> – quicker; <span class="italic-w-ipg">sukhiya</span> – happier</li>
+    <li>the superlative alternatively is expressed with the suffixes: -<span class="italic-w-ipg">issika</span> and -<span class="italic-w-ipg">iṭṭha</span>: <span class="italic-w-ipg">khippissika</span> – quickest; <span class="italic-w-ipg">pāpiṭṭha</span> – most evil; <span class="italic-w-ipg">jeṭṭha</span> – the oldest; <span class="italic-w-ipg">seṭṭha</span> – the best.</li> 
+  </ul>
+
+  <p class="paragraph-1-ipg">
+    Adverbs derived from adjectives, pronouns or nouns are called derivative adverbs. 
+    Those adverbs that can not be derived and don’t add any case endings are called pure adverbs.
+  </p>
+
+<p class="paragraph-1-ipg">Derivations occur from adjectives, pronouns or nouns:</p>
+<ul class="para-list-ipg">
+  <li>When derived from adjectives they get formed by adding the <span class="italic-w-ipg">niggahīta</span>- ending <span class="italic-w-ipg">aṃ</span> to the adjective such as: <span class="italic-w-ipg">dukkha</span>: <span class="italic-w-ipg">dukkhaṃ</span> – painful; <span class="italic-w-ipg">dīgha</span>: <span class="italic-w-ipg">dīghaṃ</span> – long, far; <span class="italic-w-ipg">sīgha</span>: <span class="italic-w-ipg">sīghaṃ</span> – fast; <span class="italic-w-ipg">pāpa</span>: <span class="italic-w-ipg">pāpaṃ</span> – evil; <span class="italic-w-ipg">sukha</span>: <span class="italic-w-ipg">sukhaṃ</span> – happily; <span class="italic-w-ipg">cira</span>: <span class="italic-w-ipg">ciraṃ</span> – for a long time;</li>
+  <li>Adverbs derived from pronouns add specific adverbial suffixes to the stem, such as: 
+    <br>-<span class="italic-w-ipg">tra: tatra</span> – there; <span class="italic-w-ipg">aññatra</span> – elsewhere;
+    <br>-<span class="italic-w-ipg">tha: sabbatha</span> – everywhere; <span class="italic-w-ipg">ubhayattha</span> – in both places;
+    <br>-<span class="italic-w-ipg">to: tato</span> – thence, from there; <span class="italic-w-ipg">yato</span> – hence, from where;
+    <br>-<span class="italic-w-ipg">dā: tadā</span> – then; <span class="italic-w-ipg">yadā</span> – when;
+    <br>-<span class="italic-w-ipg">dhi: sabbadhi</span> – everywhere;
+    <br>-<span class="italic-w-ipg">rahi: carahi</span> – now, then, therefore; <span class="italic-w-ipg">tarahi</span> – then, at that time;
+  </li>
+  <li>Nouns (and some adjectives as well) change into adverbs by adding the specific case endings: 
+    <br>-<span class="italic-w-ipg">so: dīghaso</span> – in length; <span class="italic-w-ipg">divaso</span> – by day; <span class="italic-w-ipg">bahuso</span> – often, <span class="italic-w-ipg">yoniso</span> – thoroughly, orderly;
+    <br>-<span class="italic-w-ipg">to: dakkhiṇato</span> – southern; <span class="italic-w-ipg">piṭṭhito</span> – from the back; <span class="italic-w-ipg">sabbato</span> – everywhere;
+    <br>-<span class="italic-w-ipg">dhā: ekadhā</span> – one by one;
+    <br>-<span class="italic-w-ipg">assa: cirassa</span> – at last; <span class="italic-w-ipg">kissa</span> – why?
+    <br>-<span class="italic-w-ipg">e: bahire</span> – outside; <span class="italic-w-ipg">dūre</span> – far; <span class="italic-w-ipg">santike</span> – near by 
+  </li>
+</ul>
+
+<p class="paragraph-1-ipg">Pure adverbs may ideally be learned separately. </p>
+<ul class="ul para-list-ipg">
+  <li>
+    Some of common occurrences are: <span class="italic-w-ipg">atho</span> – and, then; <span class="italic-w-ipg">anto</span> – before; <span class="italic-w-ipg">adho</span> – below; <span class="italic-w-ipg">idāni</span> – now; <span class="italic-w-ipg">kho</span> – then; <span class="italic-w-ipg">vata</span> – truly, indeed; <span class="italic-w-ipg">tiro</span> – across, beyond; <span class="italic-w-ipg">bahi</span> – outside; <span class="italic-w-ipg">paro</span> – beyond, further; <span class="italic-w-ipg">pure</span> – formerly; <span class="italic-w-ipg">hi</span> – certainly, because
+  </li>
+</ul>
+
+<p class="paragraph-1-ipg">Some grammarians  divide adverbs in groups according to their meaning instead of their derivation, such as:</p>
+<ul class="para-list-ipg">
+  <li>adverbs of time: <span class="italic-w-ipg">yada</span> – when; <span class="italic-w-ipg">ajja</span> – today; <span class="italic-w-ipg">ratto</span> – by night;</li>
+  <li>adverbs of place: <span class="italic-w-ipg">tatra</span> – there; <span class="italic-w-ipg">adho</span> – below; <span class="italic-w-ipg">idha</span> – here; <span class="italic-w-ipg">tiraṃ</span> – across</li>
+  <li>adverbs of manner: <span class="italic-w-ipg">sukhaṃ</span> – happily; <span class="italic-w-ipg">evaṃ</span> – thus; <span class="italic-w-ipg">tuṇhī</span> – silently</li>
+  <li>adverbs of quantity, degree and extent: <span class="italic-w-ipg">yāva</span> – how much; <span class="italic-w-ipg">mattaso</span> – moderately; <span class="italic-w-ipg">antamaso</span> – even;</li>
+  <li>adverbs of reason or cause: <span class="italic-w-ipg">kasmā</span> – why; <span class="italic-w-ipg">yathā</span> – thus;</li>
+  <li>adverbial particles such as: <span class="italic-w-ipg">api</span> – indeed; <span class="italic-w-ipg">iti</span> – thus;  <span class="italic-w-ipg">nāma</span> – indeed; <span class="italic-w-ipg">kho</span> – then</li>
+</ul>
+
+<p class="paragraph-1-ipg">Adverbs can denote comparisons by adding the comparative suffix -<span class="italic-w-ipg">tara</span>: <span class="italic-w-ipg">pāpatara(ṃ)</span> – more evil. The superlative does not occur.</p>
+
 
   <!-- footnotes section 9 -->
   <ul class="footnotes-container-ipg">
