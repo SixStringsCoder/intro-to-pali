80276218192ac0923735ffc477f5d5602cdc5cf4
Steve Hanlon <rshanlon@gmail.com>
2022-03-18 22:06:11 -0700

    Added tables 3 to 8 in the Pronoun comp

--- src/Pronouns.svelte
+++ src/Pronouns.svelte
@@ -15,7 +15,8 @@
 
 <p class="paragraph-1-ipg">The indefinite pronouns formed with particles <span class="italic-w-ipg">ci/cid</span> and <span class="italic-w-ipg">api/pi</span> are often used and are listed below.</p>
 
-<h4 id="section-10-1" class="subheading-1-ipg">1.	Personal Pronoun: 1st person: <span class="italic-w-ipg">ahaṃ</span> I / <span class="italic-w-ipg">mayaṃ, amhe</span> we</h4>
+<h4 id="section-10-1" class="subheading-1-ipg">1.
+  Personal Pronoun: 1st person: <span class="italic-w-ipg">ahaṃ</span> I / <span class="italic-w-ipg">mayaṃ, amhe</span> we</h4>
 
 <!-- table 1, left header and top-row header -->
 <div class="responsive-table-ipg table-regular-ipg width-table-1">
@@ -116,6 +117,383 @@
   The following tables show the declensions for the 3rd person in the respective order of demonstrative pronoun, relative pronoun and interrogative pronoun, in singular and plural number:
 </p>
 
+
+
+<h4 id="section-10-3" class="subheading-1-ipg">3.	Masculine, 3rd person, singular: <span class="italic-w-ipg">so</span>		he</h4>
+
+<!-- table 1, left header and top-row header -->
+<div class="responsive-table-ipg table-regular-ipg width-table-1">
+  <table class="vowel-list-2 table-regular-ipg width-table-1">      
+    <tr>
+      <th class="list-chart-4-ipg">case</th>
+      <th class="list-chart-4-ipg">demonstrative <br>pronoun</th>
+      <th class="list-chart-4-ipg">relative <br>pronoun</th>
+      <th class="list-chart-4-ipg">interrogative <br>pronoun</th>
+    </tr>  	
+    <tr>
+      <th class="list-chart-4-ipg"></th>
+      <td class="list-chart-3-ipg">he, that</td>
+      <td class="list-chart-3-ipg">he, who</td>
+      <td class="list-chart-3-ipg">who?</td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Nominative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">so</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yo</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">ko</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Accusative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">taṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kaṃ</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Instrumental</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tena</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yena</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kena</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Dative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tassa</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yassa</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kassa, kissa</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Ablative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tamhā, tasmā</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yamhā, yasmā</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kasmā, kismā</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Genitive</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tassa</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yassa</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kassa, kissa</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Locative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tamhi, tasmiṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yamhi, yasmiṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kamhi, kasmiṃ, kimhi, kismiṃ</span></td>
+    </tr>
+  </table>
+</div>
+
+<h4 id="section-10-4" class="subheading-1-ipg">4.	Neuter, 3rd person, singular: <span class="italic-w-ipg">yaṃ</span>		it, that</h4>
+
+<!-- table 1, left header and top-row header -->
+<div class="responsive-table-ipg table-regular-ipg width-table-1">
+  <table class="vowel-list-2 table-regular-ipg width-table-1">      
+    <tr>
+      <th class="list-chart-4-ipg">case</th>
+      <th class="list-chart-4-ipg">demonstrative <br>pronoun</th>
+      <th class="list-chart-4-ipg">relative <br>pronoun</th>
+      <th class="list-chart-4-ipg">interrogative <br>pronoun</th>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg"></th>
+      <td class="list-chart-3-ipg">it, that</td>
+      <td class="list-chart-3-ipg">that, which</td>
+      <td class="list-chart-3-ipg">which?</td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Nominative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">taṃ</span></td>      
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kiṃ</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Accusative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">taṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kiṃ</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Instrumental</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tena</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yena</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kena</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Dative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tassa</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yassa</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kassa, kissa</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Ablative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tamhā, tasmā</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yamhā, yasmā</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kasmā, kismā</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Genitive</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tassa</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yassa</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kassa, kissa</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Locative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tamhi, tasmiṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yamhi, yasmiṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kamhi, kasmiṃ, kimhi, kismiṃ</span></td>
+    </tr>
+  </table>
+</div>
+
+
+<h4 id="section-10-5" class="subheading-1-ipg">5.	Feminine, 3rd person, singular: <span class="italic-w-ipg">yā</span>			she, who</h4>
+
+<!-- table 1, left header and top-row header -->
+<div class="responsive-table-ipg table-regular-ipg width-table-1">
+  <table class="vowel-list-2 table-regular-ipg width-table-1">      
+    <tr>
+      <th class="list-chart-4-ipg">case</th>
+      <th class="list-chart-4-ipg">demonstrative <br>pronoun</th>
+      <th class="list-chart-4-ipg">relative <br>pronoun</th>
+      <th class="list-chart-4-ipg">interrogative <br>pronoun</th>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg"></th>
+      <td class="list-chart-3-ipg">she, that</td>
+      <td class="list-chart-3-ipg">she who</td>
+      <td class="list-chart-3-ipg">who?</td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Nominative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">sā</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yā</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kā</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Accusative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">taṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kaṃ</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Instrumental</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tāya</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yāya</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kāya</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Dative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tassā, tāya</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yassā, yāya</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kassā, kāya</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Ablative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tāya</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yāya</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kāya</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Genitive</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tassā, tāya</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yassā, yāya</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kassā, kāya</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Locative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tassaṃ, tāyaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yassaṃ, yāyaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kassaṃ, kāyaṃ</span></td>
+    </tr>
+  </table>
+</div>
+
+
+<h4 id="section-10-6" class="subheading-1-ipg">6.	Masculine, 3rd person, plural: <span class="italic-w-ipg">ye</span>		they</h4>
+
+<!-- table 1, left header and top-row header -->
+<div class="responsive-table-ipg table-regular-ipg width-table-1">
+  <table class="vowel-list-2 table-regular-ipg width-table-1">      
+    <tr>
+      <th class="list-chart-4-ipg">case</th>
+      <th class="list-chart-4-ipg">demonstrative <br>pronoun</th>
+      <th class="list-chart-4-ipg">relative <br>pronoun</th>
+      <th class="list-chart-4-ipg">interrogative <br>pronoun</th>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg"></th>
+      <td class="list-chart-3-ipg">they, those</td>
+      <td class="list-chart-3-ipg">they, who</td>
+      <td class="list-chart-3-ipg">who?</td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Nominative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">te</span></td>	
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">ye</span></td>	
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">ke</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Accusative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">te</span></td>	
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">ye</span></td>	
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">ke</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Instrumental</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tehi</span></td>	
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yehi</span></td>	
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kehi</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Dative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tesaṃ, tesānaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yesaṃ, yesānaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kesaṃ, kesānaṃ</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Ablative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tehi</span></td>	
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yehi</span></td>	
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kehi</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Genitive</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tesaṃ, tesānaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yesaṃ, yesānaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kesaṃ, kesānaṃ</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Locative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tesu</span></td>	
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yesu</span></td>	
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kesu</span></td>
+    </tr>
+  </table>
+</div>
+
+
+<h4 id="section-10-7" class="subheading-1-ipg">7.	Neuter, 3rd person, plural: <span class="italic-w-ipg">yāni, ye</span> 			those</h4>
+
+<!-- table 1, left header and top-row header -->
+<div class="responsive-table-ipg table-regular-ipg width-table-1">
+  <table class="vowel-list-2 table-regular-ipg width-table-1">      
+    <tr>
+      <th class="list-chart-4-ipg">case</th>
+      <th class="list-chart-4-ipg">demonstrative <br>pronoun</th>
+      <th class="list-chart-4-ipg">relative <br>pronoun</th>
+      <th class="list-chart-4-ipg">interrogative <br>pronoun</th>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg"></th>
+      <td class="list-chart-3-ipg">they, those</td>
+      <td class="list-chart-3-ipg">they, who</td>
+      <td class="list-chart-3-ipg">who?</td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Nominative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tāni, te</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yāni, ye</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kāni, ke</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Accusative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tāni, te</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yāni, ye</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kāni, ke</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Instrumental</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tehi</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yehi</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kehi</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Dative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tesaṃ, tesānaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yesaṃ, yesānaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kesaṃ, kesānaṃ</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Ablative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tehi</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yehi</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kehi</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Genitive</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tesaṃ, tesānaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yesaṃ, yesānaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kesaṃ, kesānaṃ</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Locative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tesu</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yesu</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kesu</span></td>
+    </tr>
+  </table>
+</div>
+
+
+<h4 id="section-10-8" class="subheading-1-ipg">8.	Feminine, 3rd person, plural: <span class="italic-w-ipg">tā, tāyo</span>: 	they, those</h4>
+
+<!-- table 1, left header and top-row header -->
+<div class="responsive-table-ipg table-regular-ipg width-table-1">
+  <table class="vowel-list-2 table-regular-ipg width-table-1">      
+    <tr>
+      <th class="list-chart-4-ipg">case</th>
+      <th class="list-chart-4-ipg">demonstrative <br>pronoun</th>
+      <th class="list-chart-4-ipg">relative <br>pronoun</th>
+      <th class="list-chart-4-ipg">interrogative <br>pronoun</th>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg"></th>
+      <td class="list-chart-3-ipg">those</td>
+      <td class="list-chart-3-ipg">those</td>
+      <td class="list-chart-3-ipg">who?</td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Nominative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tā, tāyo</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yā, yāyo</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kā, kāyo</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Accusative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tā, tāyo</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yā, yāyo </span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kā, kāyo</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Instrumental</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tāhi</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yāhi </span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kāhi</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Dative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tāsaṃ, tāsānaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yāsaṃ, yāsānaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kāsaṃ, kāsānaṃ</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Ablative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tāhi</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yāhi</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kāhi</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Genitive</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tāsaṃ, tāsānaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yāsaṃ, yāsānaṃ</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kāsaṃ, kāsānaṃ</span></td>
+    </tr>
+    <tr>
+      <th class="list-chart-4-ipg">Locative</th>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">tāsu</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">yāsu</span></td>
+      <td class="list-chart-3-ipg"><span class="italic-w-ipg">kāsu</span></td>
+    </tr>
+  </table>
+</div>
 
 
 
