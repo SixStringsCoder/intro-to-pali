85bb701a8f0adcc428a8a00fd3b737bb8cbb8bea
Steve Hanlon <rshanlon@gmail.com>
2022-03-19 22:29:45 -0700

    Added last content to Suffixes comp and tweaked CSS

--- src/Suffixes.svelte
+++ src/Suffixes.svelte
@@ -1,37 +1,212 @@
-<script>
-  
-</script>
-
-
 <!-- Section 13 -->
 <section id="section-13" class="section-divider-ipg">
   <h3 class="heading-2-ipg">Section Thirteen</h3>
   <h3 class="heading-3-ipg">Suffixes</h3>
+
   <p class="paragraph-1-ipg">
-    <span class="italic-w-ipg"></span>
+    It has been shown in the previous chapters that words in Pāli derive from a certain number of roots. From these roots words are developed by adding terminations, prefixes and suffixes. Terminations (suffixes) used for the conjugation of verbs and declension of nouns, pointing out gender, person, tenses, participles and cases have been introduced in the previous chapters. 
   </p>
 
   <p class="paragraph-1-ipg">
-
+    Words can get developed directly from the root (primary derivates) or from those already developed words (secondary derivates). 
+    Staying with the example of the root: <span class="italic-w-ipg">-gam-</span> primary derivates are <a href="#section13-note-52" class="notes-ipg" id="section13-note-52-rtn"><span class="italic-w-ipg">formed</span><sup class="sup-spacing-ipg">52</sup></a>:     
   </p>
+  <ul class="list-cont" id="list-cont-1">
+    <li>
+      <span class="left-list-span italic-w-ipg">gam + a: gama –</span>
+      <span class="right-list-span">1.) adj. 	going, able to go </span>
+    </li>
+    <li>
+      <span class="left-list-span"></span>
+      <span class="right-list-span no-2">2.) noun	the act of going, course</span>
+    </li>
+    <li>
+      <span class="left-list-span italic-w-ipg">gam + ana: gamana –</span>
+      <span class="right-list-span">1.) adj.		conducive to, leading to</span>
+    </li>
+    <li>
+      <span class="left-list-span"></span>
+      <span class="right-list-span no-2">2.) noun	movement, walk, journey</span>
+    </li>
+    <li>
+      <span class="left-list-span italic-w-ipg">gam + i + ka: gamika –</span> 			
+      <span class="right-list-span">1.) adj.		setting out for a journey </span>
+    </li>
+    <li>
+      <span class="left-list-span italic-w-ipg">gām + a: gāma –</span>
+      <span class="right-list-span">1.) adj. 	belonging to the village; </span>low, inferior, common </li>
+    <li>
+      <span class="left-list-span"></span>
+      <span class="right-list-span no-2">2.) noun	a village</span>
+    </li>
+  </ul>
 
-  <p class="paragraph-1-ipg">
+  <p class="paragraph-1-ipg">If prefixes or further suffixes are added to these primary derivates so called secondary derivates develop:</p>
+  <ul class="list-cont" id="list-cont-2">
+    <li>
+      <span class="left-list-span italic-w-ipg">viha + ṃ + gama: vihaṅgama  –</span> 	 	
+      <span class="right-list-span">going in the air</span>
+    </li>
+    <li>
+      <span class="left-list-span italic-w-ipg">dur + anu + gama: duranugama  –</span> 		
+      <span class="right-list-span">difficult to be followed</span>
+    </li>
+    <li>
+      <span class="left-list-span italic-w-ipg">dūra + ṃ + gama: dūraṅgama  –</span> 		
+      <span class="right-list-span">going a long way, far reaching</span>
+    </li>
+    <li>
+      <span class="left-list-span italic-w-ipg">dūra + ṃ + gami: dūraṅgami –</span>		
+      <span class="right-list-span">travelling, going far</span>
+    </li>
+    <li>
+      <span class="left-list-span italic-w-ipg">saṃ + gama: saṅgama –</span>			
+      <span class="right-list-span">going together, assembly</span>
+    </li>
+    <li>
+      <span class="left-list-span italic-w-ipg">gāma + anta: gāmanta –</span>			
+      <span class="right-list-span">the neighbourhood of a village</span>
+    </li>
+    <li>
+      <span class="left-list-span italic-w-ipg">gāma + aka: gāmaka –</span>			
+      <span class="right-list-span">a little village</span>
+    </li>
+    <li>
+      <span class="left-list-span italic-w-ipg">an + ā + gāmi + tā: anāgāmitā –</span>		
+      <span class="right-list-span">the fact of not returning</span>
+    </li>
+    <li>
+      <span class="left-list-span italic-w-ipg">gamana + bhāva: gamanabhāva –</span>		
+      <span class="right-list-span">the state of going away</span>
+    </li>
+    <li>
+      <span class="left-list-span italic-w-ipg">gamana + ā + gamana: gamanāgamana –</span>	
+      <span class="right-list-span">going & coming, rising & setting</span>
+    </li>
+  </ul>
 
-  </p>
+  <h4 id="section-10-1" class="subheading-1-ipg">1.	Suffixes of special importance</h4>
+    <p>Suffixes expressing comparison: </p>
+    <ul>
+      <li><span class="left-list-span italic-w-ipg">pāpa + tara: pāpatara –</span> 		
+        <span class="right-list-span">more evil</span>
+      </li>
+      <li><span class="left-list-span italic-w-ipg">pāpa + tama: pāpatama –</span> 		
+        <span class="right-list-span">most evil </span>
+      </li>
+      <li><span class="left-list-span italic-w-ipg">pāpa + īya: pāpīya –</span> 			
+        <span class="right-list-span">more evil</span>
+      </li>
+      <li><span class="left-list-span italic-w-ipg">pāpa + iyya: pāpiyya –</span> 		
+        <span class="right-list-span">more evil</span>
+      </li>
+      <li><span class="left-list-span italic-w-ipg">pāpa + iṭṭha: pāpiṭṭha –</span> 		
+        <span class="right-list-span">most evil</span>
+      </li>
+      <li><span class="left-list-span italic-w-ipg">pāpa + issika: pāpissika –</span> 		
+        <span class="right-list-span">most evil</span>
+      </li>
+    </ul>
 
-  <p class="paragraph-1-ipg">
+    <p>Suffixes expressing abstractness:</p>
+    <ul>
+      <li><span class="left-list-span italic-w-ipg">eka + tta: ekatta –</span> 			
+        <span class="right-list-span">oneness</span>
+      </li>
+      <li><span class="left-list-span italic-w-ipg">kata + tta: katatta –</span> 			
+        <span class="right-list-span">the fact of having done </span>
+      </li>
+    </ul>
+      
+      <p>Other suffixes expressing or forming nouns:</p>
+    <ul>
+      <li><span class="left-list-span italic-w-ipg">kār + ana: kāraṇa –</span> 			
+        <span class="right-list-span">deed, doing</span>
+      </li>
+      <li><span class="left-list-span italic-w-ipg">vāha + ana: vāhana –</span> 		
+        <span class="right-list-span">carrying </span>
+      </li>
+      <li><span class="left-list-span italic-w-ipg">vās + ant: vāsant –</span> 			
+        <span class="right-list-span">dwelling</span>
+      </li>
+      <li><span class="left-list-span italic-w-ipg">cet + anā: cetanā –</span> 			
+        <span class="right-list-span">thought</span>
+      </li>
+      <li><span class="left-list-span italic-w-ipg">sat + ya: sacca –</span> 			
+        <span class="right-list-span">truth</span>
+      </li>
+      <li><span class="left-list-span italic-w-ipg">vid + yā: vijjā –</span> 			
+        <span class="right-list-span">knowledge</span>
+      </li>
+    </ul>
 
-  </p>
+    <p>Suffixes expressing or forming agents:</p>
+    <ul>
+      <li><span class="left-list-span italic-w-ipg">para + gū: paragū –</span> 			
+        <span class="right-list-span">going to the other shore</span>
+      </li>
+      <li><span class="left-list-span italic-w-ipg">kāla + jñū: kālaññū –</span> 			
+        <span class="right-list-span">knowing the proper time </span>
+      </li>
+    </ul>
+      
+    <p>Suffixes changing or forming nouns into verbs :</p>
+    <ul>
+      <li>
+        <span class="left-list-span italic-w-ipg">taṇhā + āya + ti: taṇhāyati </span> 		
+        <span class="right-list-span">to crave</span>
+      </li>
+      <li>
+        <span class="left-list-span italic-w-ipg">mettā + āya + ti: mettāyati </span> 		
+        <span class="right-list-span">to be compassionate</span>, to love
+      </li>
+      <li>
+        <span class="left-list-span italic-w-ipg">sukha + āpe + ti: sukhāpeti </span>		
+        <span class="right-list-span">to make happy</span>
+      </li>
+      <li>
+        <span class="left-list-span italic-w-ipg">dukkha + āpe + ti: dukkhāpeti </span>	
+        <span class="right-list-span">to make miserable</span>
+      </li>
+    </ul>
+
+    <p>Suffixes forming reflexive participles:</p>
+    <ul>
+      <li>
+        <span class="left-list-span italic-w-ipg">paca + māna: pacamāna –</span> 		
+        <span class="right-list-span">cooking</span>
+      </li>
+      <li>
+        <span class="left-list-span italic-w-ipg">dadā + māna: dadāmāna –</span>		
+        <span class="left-list-span italic-w-ipg">giving</span>
+      </li>
+    </ul>
+      
+      
 
   <!-- Footnotes Section 13 -->
   <ul class="footnotes-container-ipg">
-    <li id="section10-note-52" class="footnotes-ipg"><sup>52</sup>  <a class="backarrow-ipg" href="#section10-note-52-rtn">↩</a></li>
-    <li id="section10-note-53" class="footnotes-ipg"><sup>53</sup>  <a class="backarrow-ipg" href="#section10-note-53-rtn">↩</a></li>
+    <li id="section13-note-52" class="footnotes-ipg"><sup>52</sup>Depending on the way these derivations combine with the suffixes they undergo certain changes according to the rules of sandhi vowel gradation. The suffixes can get added to the normal, the <span class="italic-w-ipg">guṇa</span> grade, where the vocalic element of the root remains unchanged. If this gets strengthened it is called <span class="italic-w-ipg">vuddhi</span> (<span class="italic-w-ipg">a</span> turns into <span class="italic-w-ipg">ā</span>; <span class="italic-w-ipg">i</span> into <span class="italic-w-ipg">e</span>……), if shortened or elided this is called <span class="italic-w-ipg">avuddhika</span> or week, reduced grade.<a class="backarrow-ipg" href="#section10-note-52-rtn">↩</a></li>
+    <li id="section13-note-53" class="footnotes-ipg"><sup>53</sup>See also under causative verbs.<a class="backarrow-ipg" href="#section10-note-53-rtn">↩</a></li>
   </ul>
 </section>
 <!-- /Section 13 -->
 
 
 <style>
-  
+  ul {
+    margin: 0;
+  }
+
+  ul#list-cont-1 li:nth-child(2) {
+    width: 100%;
+  }
+
+  ul#list-cont-1 li span.no-2 {
+    margin-left: 140px;
+  }
+
+  p {
+    margin: 10px 0 10px 30px;
+  }
 </style>
