d21b676b0552d36d8c64ed14a97ee40e349340e1
Steve Hanlon <rshanlon@gmail.com>
2022-03-22 17:11:15 -0700

    Added custom CS for select menu

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -10,13 +10,24 @@
 
 <nav class="custom-select">
 	<button id="prev-arrow" class="btn-arrow" on:click={() => dispatch('prev')}>&#8592;</button>
+
+	<span class="custom-dropdown small">
+    
+    <select on:change bind:value={index}>
+			<option value="" disabled>Go to section:</option> 
+			{#each menuOptions as linkName, i}
+		<option value={i}>{linkName}</option>
+		{/each}
+			</select>
+		</span>
+
 	
-	<select on:change bind:value={index}>
+	<!-- <select on:change bind:value={index}>
 		<option value="" disabled>Go to section:</option> 
 			{#each menuOptions as linkName, i}
 		<option value={i}>{linkName}</option>
 		{/each}
-	</select>
+	</select> -->
 	
 	<button id="next-arrow" class="btn-arrow" on:click={() => dispatch('next')}>&#8594;</button>
 </nav>
@@ -30,12 +41,51 @@
 		justify-content: center;
 		align-items: center;
 	}
+
+	/* custom select menu */
+	.big {
+		font-size: 1.2em;
+	}
+
+	.small {
+		font-size: .7em;
+	}
+
+	/* Custom dropdown */
+	.custom-dropdown {
+		position: relative;
+		display: inline-block;
+		vertical-align: middle;
+		margin: 10px; /* demo only */
+	}
+
+	.custom-dropdown select {
+		background-color: #bc561b;
+		color: #fff;
+		font-size: inherit;
+		padding: .5em;
+		border: 0;
+		margin: 0;
+		border-radius: 3px;
+		text-indent: 0.01px;
+		text-overflow: '';
+		-webkit-appearance: button; /* hide default arrow in chrome OSX */
+	}
+
+	.custom-dropdown::before,
+	.custom-dropdown::after {
+		content: "";
+		position: absolute;
+		pointer-events: none;
+	}
+
+	
 	
 
 	button.btn-arrow {
 		background-color: hsl(41, 36%, 20%);
 		color: white;
-		margin: 0 20px;
+		margin: 0 10px;
 		padding: 3px 5px 5px;
 		font-size: 1.1rem;
 		border-radius: 50%;
